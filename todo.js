79142f6302ce4bec3032df4137f29e4880be3204
Josh Ladwig <joshua_ladwig@yahoo.com>
2018-05-21 21:57:02 +0000

    todo app connected to database

--- todo.js
+++ todo.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
-var nunjucks = require('nunjucks');
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const body_parser = require('body-parser');
+const pgp = require('pg-promise')({});
+const db = pgp({database: 'todoapp', user: 'postgres'});
+app.use(express.static('public'));
+app.use(body_parser.urlencoded({extended: false}));
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -17,11 +22,35 @@ app.get('/', function(req, resp) {
 });
 
 app.get('/todos', function(req, resp) {
+    var dbInfo = db.query('SELECT * FROM task')
+        .then(function() {
+            console.log('completed');
+        })
+        .catch(function(err){
+            console.error(err);
+        });
+    console.log("what is dbInfo?", dbInfo);
     var context = {
         title: "To Do List",
         content: "Here's a list of To Dos"
     };
-        resp.render('todo.html', context);
+    resp.render('todo.html', context);
+});
+app.post('/todos', function (req, resp) {
+    //get the entered task and store into db
+    var newTask = req.body.task;
+    if(newTask) {
+        db.result('INSERT INTO task (id, description, done) VALUES(default, $1, false)', newTask)
+            .then(function(){
+                console.log("task written to database");
+            })
+            .catch(function(err){
+                console.error(err);
+            });
+    }
+  //probably best to just do a redirect back to todos and 
+  //add compute the todos page from that app handler
+  resp.redirect('/todos');
 });
 
 app.get('/todos/add', function(req, resp) {
@@ -29,7 +58,7 @@ app.get('/todos/add', function(req, resp) {
         title: "Add Item",
         content: "Here's a form to add an item."
     };
-        resp.render('addToDo.html', context);
+    resp.render('addToDo.html', context);
 });
 
 app.get('/todo/done/:id', function(req, resp) {
